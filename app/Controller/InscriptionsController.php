5a00d7b85395efaf8e6a58c4d7d66d08ce38d775
joseph <jardix22@gmail.com>
2013-02-01 01:15:13 -0500

    lol

--- app/Controller/InscriptionsController.php
+++ app/Controller/InscriptionsController.php
@@ -8,6 +8,28 @@ class InscriptionsController extends AppController
 
 	public function add()
 	{
+		if ($this->request-is('post')) {
+			if ($this->Inscription->saveAssociated($this->request->data)) {
+				$this->Session->setFlash(
+					array('message' => 'Su inscripcion ha sido realizada exitosamente.','type' => 'alert-success'),
+					'bootstrap_message'
+				);
+			} else {
+				$this->Session->setFlash(
+					array('message' => 'Ha ocurrido un error, vuelve a intentarlo.','type' => 'alert-error'),
+					'bootstrap_message'
+				);
+			}
+		}
+	}
+
+	public function show_result($value='')
+	{
+			
+	}
+
+	public function printForm($value='')
+	{
 		
 	}
 }
---
+++ app/View/Elements/bootstrap_message.ctp
@@ -0,0 +1,4 @@
+<? $class = 'alert '.$message['type']; ?>
+<div class=<?php echo "'".$class."'"; ?> >
+	<?php echo $message['message']; ?>
+</div>
